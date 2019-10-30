859e048bad02b153505e60b4f2b4261f8e2feb00
df-ccep <56944653+df-ccep@users.noreply.github.com>
2019-10-30 17:33:30 -0400

    Added comments

--- normalize_ekg.py
+++ normalize_ekg.py
@@ -3,7 +3,9 @@ import numpy as np
 ekg = np.load("F:\\30731_ekg_data\\ekg_lead_1.npy")
 
 ekg = ekg[400000:401250]
+#EKG values all between -1 and 1
 ekg = ekg / np.max(np.abs(ekg))
+#So I can copy-paste into JavaScript (It's not letting me upload files directly from JS)
 print([e for e in ekg], end = ', ')
 
 #np.save(ekg, "ekg_for_js")
