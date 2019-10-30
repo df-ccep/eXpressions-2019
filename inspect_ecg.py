6d43df820c2f03ca92e9a3dd2b36609513b66a91
df-ccep <56944653+df-ccep@users.noreply.github.com>
2019-10-30 17:30:38 -0400

    Update inspect_ecg.py

--- inspect_ecg.py
+++ inspect_ecg.py
@@ -2,10 +2,9 @@
 """
 Created on Mon Sep 23 20:52:19 2019
 
-@author: kids
+@author: dfertel
 """
 import numpy as np
-
 
 path = 'C:\\Users\\kids\\Expressions\\'
 
@@ -13,25 +12,32 @@ ecg = np.load(path + '30731_ekg_data\\ekg_lead_1.npy')
 
 print(max(ecg))
 print(min(ecg))
-ecg[ecg > 200] = 200
-ecg[ecg < -200] = -200
 
-ecg = ecg[4000:5000]
+#All numbers greater than 200 to 200
+ecg[ecg > 200] = 200
+#All numbers less than -200 to 200
+ecg[ecg < -200] = -200
+#take a sample from the EKG
+ecg = ecg[400000:401250]
+#map notes to numbers
 notes = {1:'a3', 2:'b3', 3:'c4', 4:'d4', 5:'e4', 6:'f4', 7:'g4', 8:'a4', 9:'b4',
          10:'c5', 11:'d5', 12:'e5', 13:'f5', 14:'g5'}
-
+#divide the range up into equal bins
 bins = np.linspace(-200, 200, 14)
 print(bins)
+#bin the EKG
 binned = np.digitize(ecg, bins)
 print(binned)
 note = []
 
 for x in binned:
 #    print(notes[x], end = ' ')
+    #convert numbers to notes
     note.append(notes[x])
     
 print(note, end = ' ')  
 print(np.bincount(binned))
 
 np.reshape(note, (50, 20))
+#save as a comma-separated values file
 np.savetxt(path + '1000_notes.csv', note, fmt = '%s', delimiter = ', ')
